847a21de105cca1a0f6b470e7da481791fa00624
leminhtuan96 <tuanmokc@gmail.com>
2022-12-27 01:52:59 +0700

    update creen record

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,50 @@
     <title>Screen Recorder</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 </head>
+<script>
+    function change () // no ';' here
+    {
+        var elem = document.getElementById('myButton')
+        if (elem.value == 'REC') 
+        {
+elem.addEventListener("click", async function () {
+let stream = await navigator.mediaDevices.getDisplayMedia({
+video: true
+})
+//needed for better browser support
+const mime = MediaRecorder.isTypeSupported("video/webm; codecs=vp9")
+? "video/webm; codecs=vp9"
+: "video/webm"
+let mediaRecorder = new MediaRecorder(stream, {
+mimeType: mime
+})
+let chunks = []
+mediaRecorder.addEventListener('dataavailable', function(e) {
+chunks.push(e.data)
+})
+mediaRecorder.addEventListener('stop', function(){
+let blob = new Blob(chunks, {
+type: chunks[0].type
+})
+let url = URL.createObjectURL(blob)
+let video = document.querySelector("video")
+video.src = url
+let a = document.createElement('a')
+a.href = url
+a.download = 'video.webm'
+a.click()
+})
+//we have to start the recorder manually
+mediaRecorder.start()
+})}
+        else
+        if (elem.value == 'STOP') 
+        {elem.value = 'REC'}
+    }
 
+
+
+</script>
 <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
