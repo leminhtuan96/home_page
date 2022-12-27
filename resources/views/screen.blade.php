3d7fe4840ec3c3511016224c05cab12502795a8d
leminhtuan96 <tuanmokc@gmail.com>
2022-12-28 06:06:09 +0700

    update push javascript

--- resources/views/screen.blade.php
+++ resources/views/screen.blade.php
@@ -1,4 +1,6 @@
 @extends('welcome')
+@stack('screen-record')
+
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
